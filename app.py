545d24a3a404ae0c9b4f5fe6c59b70d677985629
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-05-15 11:43:29 -0400

    [EDIT] Extended main app file with SQL access code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
@@ -17,3 +17,25 @@ class User(db.Model):
 def index():
     users = User.query.all()
     return render_template("users.html", users=users)
+
+@app.route("/add_user", methods=["GET", "POST"])
+def add_user():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        age = request.form["age"]
+        user = User(username=username, email=email, age=age)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("index"))
+    else:
+        return render_template("add_user.html")
+    
+@app.route("/delete_users", methods=["POST"])
+def delete_users():
+    db.drop_all()
+    db.create_all()
+    return redirect(url_for("index"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
